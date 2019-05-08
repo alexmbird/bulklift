4fd67befbef17ccd42a5911b8520afd92591cbe5
Alex Hewson <alex@mbird.biz>
2019-05-08 14:25:45 +0100

    Python version check; --debug for noisy error output

--- code/cli.py
+++ code/cli.py
@@ -12,4 +12,6 @@ EPILOG = """
 parser = argparse.ArgumentParser(description=DESCRIPTION, epilog=EPILOG)
 
 parser.add_argument('source_tree_root', type=str, nargs=1,
-                     help="root path for your source tree; must contain a .bulklift.yaml with root=true")
+                    help="root path for your source tree; must contain a .bulklift.yaml with root=true")
+parser.add_argument('--debug', action='store_true',
+                    help="debugging output")
--- code/main.py
+++ code/main.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
 import sys
-import shutil
+from pathlib import Path
 
 from cli import parser
 from source import MediaSourceNode
-from util import is_executable
+
+
+MIN_PYTHON_VERSION = (3,6)
 
 
 if __name__ == '__main__':
+
+  if sys.version_info < MIN_PYTHON_VERSION:
+    sys.exit("Fatal: at least Python {} required".format(
+      '.'.join(map(str,MIN_PYTHON_VERSION)))
+    )
+
   args = parser.parse_args()
-  tree_root = MediaSourceNode(args.source_tree_root[0], parent=None)
-  tree_root.walk()
+
+  try:
+    tree_root = MediaSourceNode(Path(args.source_tree_root[0]), parent=None)
+    tree_root.walk()
+  except Exception as e:
+    if args.debug:
+      raise
+    else:
+      sys.exit("Fatal: {}".format(e))
